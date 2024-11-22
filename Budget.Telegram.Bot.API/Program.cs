4119e0bbf75ebbe24b0a87fcab7e73a8ad2b7c56
Vlad Slupko <vad.slupko.ukr@gmail.com>
2024-11-22 21:46:26 +0200

    Created project

---
+++ Budget.Telegram.Bot.API/Program.cs
@@ -0,0 +1,34 @@
+namespace Budget.Telegram.Bot.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.Run();
+    }
+}
