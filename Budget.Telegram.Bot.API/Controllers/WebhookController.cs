d6b653f4c8075c543269ff8b1ff707a99bd3dd58
Vlad Slupko <vad.slupko.ukr@gmail.com>
2024-11-23 13:23:34 +0200

    config to use ngrok

--- Budget.Telegram.Bot.API/Controllers/WebhookController.cs
+++ Budget.Telegram.Bot.API/Controllers/WebhookController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -9,23 +10,43 @@ namespace Budget.Telegram.Bot.API.Controllers;
 public class WebhookController : ControllerBase
 {
     private readonly ITelegramBotClient _botClient;
+    private readonly ILogger<WebhookController> Logger;
 
-    public WebhookController(ITelegramBotClient botClient)
+    public WebhookController(ITelegramBotClient botClient, ILogger<WebhookController> logger)
     {
         _botClient = botClient;
+        Logger = logger;
     }
     
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] Update update)
     {
-        if (update.Message?.Text != null)
+        if (update == null)
         {
-            var chatId = update.Message.Chat.Id;
-            var messageText = update.Message.Text;
-
-            await _botClient.SendMessage(chatId, $"You said: {messageText}");
+            Logger.LogError("Received an empty update");
+            return BadRequest();
         }
 
-        return Ok();
+        try
+        {
+            Logger.LogInformation($"Received update: {update.Id}");
+
+            if (update.Message != null)
+            {
+                var chatId = update.Message.Chat.Id;
+                var messageText = update.Message.Text;
+
+                Logger.LogInformation($"Message from {chatId}: {messageText}");
+
+                await _botClient.SendMessage(chatId, $"You said: {messageText}");
+            }
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError($"Error processing update: {ex.Message}");
+            return StatusCode(500, "Internal server error");
+        }
     }
 }
