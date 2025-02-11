e0b3633ee0f3a8fb60acaab7e333b916cae8ffb1
Vlad Slupko <vad.slupko.ukr@gmail.com>
2025-02-11 23:07:57 +0200

    revived project

--- Budget.Telegram.Bot.API/Program.cs
+++ Budget.Telegram.Bot.API/Program.cs
@@ -7,6 +7,9 @@ using Budget.Telegram.Bot.Infrastructure.Business.Services.BotManagementServices
 using Budget.Telegram.Bot.Infrastructure.Business.Services.Helpers;
 using Budget.Telegram.Bot.Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
 using Telegram.Bot;
 
 namespace Budget.Telegram.Bot.API;
@@ -16,52 +19,100 @@ public abstract class Program
     public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        var token = builder.Configuration["TelegramBot:Token"];
-        var webhookUrl = builder.Configuration["TelegramBot:WebhookUrl"];
-
-        builder.Logging.ClearProviders();
-        builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-
-        builder.Services.AddControllers();
-        builder.Services.ConfigureTelegramBot<Microsoft.AspNetCore.Mvc.JsonOptions>(options => options.JsonSerializerOptions);
-
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
         
-        builder.Services.AddSingleton<ITelegramBotClient>(options => new TelegramBotClient(token));
-        builder.Services.AddSingleton<IBotSessionStateService, BotSessionStateService>();
-        builder.Services.AddSingleton<BotMenusConfig>();
+        #region Serilog Logger
+        
+        var logsConnectionString = builder.Configuration.GetConnectionString("LogsConnection");
 
-        builder.Services.AddScoped<ITelegramUserService, TelegramUserService>();
-        builder.Services.AddScoped<IUsersGroupService, UsersGroupService>();
-        builder.Services.AddScoped<IBudgetService, BudgetService>();
-        builder.Services.AddScoped<IDepositService, DepositService>();
-        builder.Services.AddScoped<IExpenseService, ExpenseService>();
-        builder.Services.AddScoped<IBotMenuManagementService, BotMenuManagementService>();
-        builder.Services.AddScoped<IBotGroupManagementService, BotGroupManagementService>();
-        builder.Services.AddScoped<IBotBudgetManagementService, BotBudgetManagementService>();
-        builder.Services.AddScoped<IBotHelper, BotHelper>();
-        builder.Services.AddScoped<BotHandler>();
-
-        builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
+        if (builder.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+                .CreateLogger();
         }
 
-        app.UseHttpsRedirection();
-        app.UseAuthorization();
-        app.MapControllers();
+        if (builder.Environment.IsProduction())
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+                .WriteTo.MSSqlServer(
+                    connectionString: logsConnectionString,
+                    sinkOptions: new MSSqlServerSinkOptions
+                    {
+                        TableName = "Logs",
+                        AutoCreateSqlTable = true
+                    },
+                    restrictedToMinimumLevel: LogEventLevel.Warning)
+                .CreateLogger();
+        }
         
-        var botClient = app.Services.GetRequiredService<ITelegramBotClient>();
-        await botClient.SetWebhook($"{webhookUrl}/api/bot/Webhook");
+        builder.Logging.ClearProviders();
+        builder.Host.UseSerilog();
+        
+        #endregion
+        
+        Log.Warning("Starting web host");
 
-        await app.RunAsync();
+        try
+        {
+            var token = builder.Configuration["TelegramBot:Token"];
+            var webhookUrl = builder.Configuration["TelegramBot:WebhookUrl"];
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+            builder.Services.AddControllers();
+            builder.Services.ConfigureTelegramBot<Microsoft.AspNetCore.Mvc.JsonOptions>(options => options.JsonSerializerOptions);
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            
+            builder.Services.AddSingleton<ITelegramBotClient>(options => new TelegramBotClient(token));
+            builder.Services.AddSingleton<IBotSessionStateService, BotSessionStateService>();
+            builder.Services.AddSingleton<BotMenusConfig>();
+
+            builder.Services.AddScoped<ITelegramUserService, TelegramUserService>();
+            builder.Services.AddScoped<IUsersGroupService, UsersGroupService>();
+            builder.Services.AddScoped<IBudgetService, BudgetService>();
+            builder.Services.AddScoped<IDepositService, DepositService>();
+            builder.Services.AddScoped<IExpenseService, ExpenseService>();
+            builder.Services.AddScoped<IBotMenuManagementService, BotMenuManagementService>();
+            builder.Services.AddScoped<IBotGroupManagementService, BotGroupManagementService>();
+            builder.Services.AddScoped<IBotBudgetManagementService, BotBudgetManagementService>();
+            builder.Services.AddScoped<IBotHelper, BotHelper>();
+            builder.Services.AddScoped<BotHandler>();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            
+            var botClient = app.Services.GetRequiredService<ITelegramBotClient>();
+            await botClient.SetWebhook($"{webhookUrl}/api/bot/Webhook");
+
+            await app.RunAsync();
+        }
+        catch (Exception e)
+        {
+            Log.Fatal(e, e.Message);
+        }
+        finally
+        {
+            Log.Warning("Web host shutdown");
+            await Log.CloseAndFlushAsync();
+        }
     }
 }
